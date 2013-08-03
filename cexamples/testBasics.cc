87310e97c99ca1afb6b703031adce9307100d09e
vineetsk <vineetsk@gmail.com>
2013-08-04 00:56:20 +0530

    added google marco examples

--- cexamples/testBasics.cc
+++ cexamples/testBasics.cc
@@ -10,9 +10,24 @@ class TestBasics : public testing::Test{
 	}
 };
 
-TEST_F(TestBasics, frameworkUsage){
+TEST_F(TestBasics, googleTestMacroUsage){
 	EXPECT_TRUE(true);
+	EXPECT_FALSE(false);
+	
 	EXPECT_EQ(1,1);
+	EXPECT_NE(0,1);
+	
+	EXPECT_LT(1,2);
+	EXPECT_GT(2,1);
+	
+	EXPECT_LE(2,2);
+	EXPECT_GE(2,2);
+	
+	EXPECT_STREQ("same", "same");
+	EXPECT_STRCASEEQ("same", "SaMe");
+	
+	EXPECT_STRNE("same", "not same");
+	EXPECT_STRCASENE("same", "Not Same");
 }
 
 TEST_F(TestBasics, sizeOfChar){
